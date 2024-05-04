273ab00e8df7fb3820e11721d6372d6fd8e1929d
SwiftRobertPietraru <r.pietraru@pointesoftware.com>
2024-05-04 16:35:28 +0300

    settings UI

--- src/routes/app/settings/+page.server.ts
+++ src/routes/app/settings/+page.server.ts
@@ -8,11 +8,6 @@ export const load = (async ({ locals }) => {
      const user = locals.pb.authStore.model;
      if (!user) redirect(303, '/auth/login');
 
-     const profiles = await locals.pb.collection('profile').getList(1, 1, { filter: `creator="${user.id}"` });
-     if (profiles.items.length === 0) redirect(303, '/profile/creation');
-     const profile = profiles.items[0];
-
-
-     redirect(303, `/app/${profile.id}`);
+     redirect(303, `/app/settings/account`);
 });
      
---
+++ src/routes/app/settings/account/+page.server.ts
@@ -0,0 +1,26 @@
+import { superValidate } from "sveltekit-superforms";
+import { zod } from "sveltekit-superforms/adapters";
+import { type Actions, fail } from "@sveltejs/kit";
+import type { PageServerLoad } from "./$types.js";
+import { accountFormSchema } from "./account-form.svelte";
+
+export const load: PageServerLoad = async () => {
+	console.log('loading')
+	return {
+		form: await superValidate(zod(accountFormSchema)),
+	};
+};
+
+export const actions: Actions = {
+	default: async (event) => {
+		const form = await superValidate(event, zod(accountFormSchema));
+		if (!form.valid) {
+			return fail(400, {
+				form,
+			});
+		}
+		return {
+			form,
+		};
+	},
+};
